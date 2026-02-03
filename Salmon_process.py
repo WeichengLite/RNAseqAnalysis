3bb50f43cb66493789379579c54b6026c3d06210
Weicheng Li <104164244+WeichengLite@users.noreply.github.com>
2026-02-03 12:09:22 -0800

    Add conda environment setup instructions
    Added instructions for setting up conda environment for MacBook users.

---
+++ Salmon_process.py
@@ -0,0 +1,7 @@
+# For MacBook users, highly recommend having its own environment with conda
+
+#To check if you're in the correct conda environment:
+conda env list
+#The active environment will have an asterisk (*) next to it. You want to be in the rna-seq environment.
+#If you're not in it, activate it first:
+conda activate rna-seq
